cf67de1e419da92d2d12f66e9f6583d9a620046f
BrendowAlisson <89408708+BrendowAlisson@users.noreply.github.com>
2022-09-28 10:33:16 -0300

    test

---
+++ read_msg.py
@@ -0,0 +1,11 @@
+import socket
+
+UDP_IP = "fd50:638d:bfa7:0:0:0:0:1"
+UDP_PORT = 113
+
+sock = socket.socket(socket.AF_NET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+  data, addr = sock.recvfrom(1024)
+  print("received message:", data)
